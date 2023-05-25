ddd6aceccb2dc49f62d5b73c047c4b1e384fdc06
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-05-25 10:04:10 +0100

    setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='container grid grid-cols-12'>
+			<div className='col-span-12 md:col-span-8'></div>
+			<div className='col-span-12 md:col-span-4'></div>
+		</div>
+	);
 }
 
 export default App;
