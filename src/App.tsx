e185d2eef554e0e37d5da58566816fd3718bdc1b
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-05-25 14:32:11 +0100

    update: done with form header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,25 @@ import './styles/index.scss';
 function App() {
 	return (
 		<div className='container grid grid-cols-12'>
-			<div className='col-span-12 md:col-span-8'></div>
+			<div className='col-span-12 md:col-span-8'>
+				<div className='form'>
+					<header className='form-header'>
+						<div className='logo'>
+							<span className='logo-circle'></span>
+							<h1>
+								AceCoin <span className='light'>Pay</span>
+							</h1>
+						</div>
+						<div className='timer'>
+							<span className='timer-box'>0</span>
+							<span className='timer-box'>1</span>
+							<span className='timer-divider'>:</span>
+							<span className='timer-box'>1</span>
+							<span className='timer-box'>9</span>
+						</div>
+					</header>
+				</div>
+			</div>
 			<div className='col-span-12 md:col-span-4'></div>
 		</div>
 	);
